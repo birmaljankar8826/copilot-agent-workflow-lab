7c2ef3fa5b648f9f0f8ce5aabc42eb766c89f27c
Jankar <b.ramling.jankar@accenture.com>
2026-06-19 18:17:01 +0530

    Updated app.js file

--- src/app.js
+++ src/app.js
@@ -2,11 +2,16 @@ function divide(a, b) {
     return a / b;
 }
 
+function mul(a, b) {
+    return a * b;
+}
+
 function getUser(id) {
     return users[id];
 }
 
 module.exports = {
     divide,
+    mul,
     getUser
 };
