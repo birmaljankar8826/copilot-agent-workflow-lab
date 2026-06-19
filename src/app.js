da42e0ef8b4efe0ce4a4721bebbbf9c8a00bc021
Jankar <b.ramling.jankar@accenture.com>
2026-06-19 22:14:50 +0530

    App.js file code changes

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 function divide(a, b) {
-    if (b === 0) {
-        throw new Error("Division by zero");
-    }
-    return a / b;
+    return a / 0;
 }
 
 function mul(a, b) {
-    return a * b;
+    return a * 0;
 }
 
-const users = {};
 
 function getUser(id) {
     return users[id];
